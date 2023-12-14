9cc1d4e16559544b9d1d36e70af3fc50d8909ec9
jianghan <wuxubaiyang@gmail.com>
2023-12-14 09:54:03 +0800

    增加权限弹窗全选

--- lib/widget/dialog/permission_picker.dart
+++ lib/widget/dialog/permission_picker.dart
@@ -48,7 +48,12 @@ class PermissionPickerDialog extends StatelessWidget {
         final provider = context.read<PermissionPickerDialogProvider>();
         return CustomDialog(
           content: _buildContent(context),
-          title: Text('${platform.name}权限'),
+          title: Selector<PermissionPickerDialogProvider, int>(
+            selector: (_, provider) => provider.selectPermissions.length,
+            builder: (_, count, __) {
+              return Text('${platform.name}权限（$count）');
+            },
+          ),
           constraints: const BoxConstraints.tightFor(width: 340),
           actions: [
             TextButton(
@@ -69,6 +74,7 @@ class PermissionPickerDialog extends StatelessWidget {
   // 构建内容
   Widget _buildContent(BuildContext context) {
     final controller = TextEditingController();
+    final provider = context.read<PermissionPickerDialogProvider>();
     return FutureProvider<List<PlatformPermissionTuple>?>(
       initialData: null,
       create: (_) => ProjectTool.getFullPermissions(platform),
@@ -85,17 +91,32 @@ class PermissionPickerDialog extends StatelessWidget {
                         .where((e) => e.search(controller.text))
                         .toList()
                     : permissions;
-                return Column(children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: SearchBar(
-                      hintText: '输入过滤条件',
-                      controller: controller,
-                      onChanged: (v) => setState(() {}),
-                    ),
-                  ),
-                  Expanded(child: _buildPermissionList(context, temp)),
-                ]);
+                return Selector<PermissionPickerDialogProvider,
+                    List<PlatformPermissionTuple>>(
+                  selector: (_, provider) => provider.selectPermissions,
+                  builder: (_, selectPermissions, __) {
+                    final checked = (temp.length != selectPermissions.length)
+                        ? (selectPermissions.isEmpty ? false : null)
+                        : true;
+                    return Column(children: [
+                      CheckboxListTile(
+                        value: checked,
+                        tristate: true,
+                        title: SearchBar(
+                          hintText: '输入过滤条件',
+                          controller: controller,
+                          onChanged: (v) => setState(() {}),
+                        ),
+                        onChanged: (v) => provider.selectPermissions =
+                            !(checked ?? false) ? permissions : [],
+                      ),
+                      Expanded(
+                        child: _buildPermissionList(
+                            context, temp, selectPermissions),
+                      ),
+                    ]);
+                  },
+                );
               },
             );
           },
@@ -106,46 +127,53 @@ class PermissionPickerDialog extends StatelessWidget {
 
   // 构建权限列表
   Widget _buildPermissionList(
-      BuildContext context, List<PlatformPermissionTuple> permissions) {
+    BuildContext context,
+    List<PlatformPermissionTuple> permissions,
+    List<PlatformPermissionTuple> selectPermissions,
+  ) {
     final provider = context.read<PermissionPickerDialogProvider>();
-    return Selector<PermissionPickerDialogProvider,
-        List<PlatformPermissionTuple>>(
-      selector: (_, provider) => provider.selectPermissions,
-      builder: (_, selectPermissions, __) {
-        return ListView.separated(
-          shrinkWrap: true,
-          itemCount: permissions.length,
-          separatorBuilder: (_, __) => const Divider(),
-          itemBuilder: (_, i) {
-            final item = permissions[i];
-            return CheckboxListTile(
-              value: selectPermissions.contains(item),
-              onChanged: (_) => provider.selectPermission(item),
-              title:
-                  Text(item.name, maxLines: 1, overflow: TextOverflow.ellipsis),
-              subtitle: Text(
-                item.desc,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: Theme.of(context)
-                    .textTheme
-                    .bodySmall
-                    ?.copyWith(color: Colors.grey),
-              ),
-            );
-          },
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: permissions.length,
+      separatorBuilder: (_, __) => const Divider(),
+      itemBuilder: (_, i) {
+        final item = permissions[i];
+        return CheckboxListTile(
+          value: selectPermissions.contains(item),
+          onChanged: (_) => provider.selectPermission(item),
+          title: Text(item.name, maxLines: 1, overflow: TextOverflow.ellipsis),
+          subtitle: Text(
+            item.desc,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall
+                ?.copyWith(color: Colors.grey),
+          ),
         );
       },
     );
   }
 }
 
+/*
+* 权限选择列表弹窗数据提供者
+* @author wuxubaiyang
+* @Time 2023/12/14 9:38
+*/
 class PermissionPickerDialogProvider extends BaseProvider {
   // 已选权限集合
   List<PlatformPermissionTuple> _selectPermissions;
 
   // 获取已选权限集合
   List<PlatformPermissionTuple> get selectPermissions => _selectPermissions;
+
+  // 设置已选权限集合
+  set selectPermissions(List<PlatformPermissionTuple> value) {
+    _selectPermissions = value;
+    notifyListeners();
+  }
 
   PermissionPickerDialogProvider(super.context,
       {required List<PlatformPermissionTuple> permissions})
